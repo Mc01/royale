7b0c1de6045a14249f7cfa3816eec517f8ed7558
Mc <phenom.home@gmail.com>
2017-09-10 10:26:11 +0200

    Add basic constructor.

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,5 +1,28 @@
 pragma solidity ^0.4.11;
 
 contract Game {
-  
+  uint256 feeMultiplier = 1 finney;
+  uint256 delayMultipler = 1 hours;
+
+  uint256 public noPlayers;
+  uint256 public noGames;
+  uint256 public basicFee;
+  uint256 public gameDelay;
+
+  function Game(
+    uint256 _noPlayers,
+    uint256 _noGames,
+    uint256 _basicFee,
+    uint256 _gameDelay
+  ) {
+    require(_noPlayers >= 100 && _noPlayers <= 100000); // 100 to 100k
+    require(_noGames >= 3 && _noGames <= 30); // 3 to 30
+    require(_basicFee >= 1 && _basicFee <= 1000); // 1 finney to 1 eth
+    require(_gameDelay >= 1 && _gameDelay <= 24); // 1 hour to 1 day
+
+    noPlayers = _noPlayers;
+    noGames = _noGames;
+    basicFee = _basicFee;
+    gameDelay = _gameDelay;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,34 @@
 const Game = artifacts.require('Game.sol');
 
 contract('Game', (accounts) => {
-  
+  const noPlayers = 1000;
+  const noGames = 30;
+  const basicFee = 1;
+  const gameDelay = 1;
+
+  beforeEach(async () => {
+    game = await Game.new(noPlayers, noGames, basicFee, gameDelay);
+  })
+
+  it('test new game', async () => {
+    assert.equal(
+      await game.noPlayers(), noPlayers,
+      'Players should match'
+    );
+
+    assert.equal(
+      await game.noGames(), noGames,
+      'Games should match'
+    );
+
+    assert.equal(
+      await game.basicFee(), basicFee,
+      'Fee should match'
+    );
+
+    assert.equal(
+      await game.gameDelay(), gameDelay,
+      'Delay should match'
+    );
+  })
 })
